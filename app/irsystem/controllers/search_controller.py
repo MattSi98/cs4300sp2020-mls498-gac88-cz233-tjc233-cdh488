f479fdbe3d13e7f0e4c93c6c1926585fa2b578c7
MattSi98 <mls498@cornell.edu>
2020-04-27 20:36:37 -0400

    dans work

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -7,10 +7,10 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 import json
 import numpy as np
 import random
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
 
 
-
-###########  this is the trending US topics for the day #####################################
 import pandas as pd
 from pytrends.request import TrendReq
 pytrend = TrendReq()
@@ -20,8 +20,6 @@ df = pytrend.trending_searches(pn='united_states')
 trending = []
 for x in df.values:
     trending.append(x[0])
-
-
 
 project_name = "Spotify Buddy Playlist Generation"
 net_id = "Matthew Simon: mls498, Grayson Campbell: gac88, Daniel Hayon: dh488, Theo Carrel: tjc233, Carol Zhang: cz233"
@@ -104,6 +102,77 @@ def getTwentyRandQuestions(validQuestions):
 		return finalQuestions
 
 
+
+
+def cosineSim(sentence1, sentence2):
+    tmp1 = []
+    tmp2 = []
+
+    sent1_set = set(sentence1)
+    sent2_set = set(sentence2)
+    union = sent1_set.union(sent2_set)
+
+    for word in union:
+        if word in sentence1:
+            tmp1.append(1)
+        else:
+            tmp1.append(0)
+        if word in sentence2:
+            tmp2.append(1)
+        else:
+            tmp2.append(0)
+
+    c = 0
+  
+    # cosine formula  
+    for i in range(len(union)): 
+            c+= tmp1[i]*tmp2[i] 
+    cosine = c / float((sum(tmp1)*sum(tmp2))**0.5) 
+    return cosine
+
+
+
+
+'''
+Takes a random question and then uses cosine similarity to select the 20 most similar questions to it
+
+motivation from: https://www.geeksforgeeks.org/python-measure-similarity-between-two-sentences-using-cosine-similarity/
+'''
+def getMostSimilar(validQuestions):
+    if validQuestions == []:
+        return ["invlaid input - try again."]
+    else:
+        randNum = random.randrange(0, len(validQuestions))
+        init_qs = []
+        stoplisted = []
+        for x in validQuestions:
+            init_qs.append(word_tokenize(x[0]))
+
+        randomized_start_word = init_qs[randNum]
+        ordered_list = []
+        for idx, val in enumerate(init_qs):
+            if val != randomized_start_word:
+                tmp = cosineSim(randomized_start_word, val)
+                ordered_list.append((tmp,idx))
+    ordered_list.sort()
+    ordered_list.reverse()
+    final = []
+    for x in ordered_list[:20]:
+        final.append((validQuestions[x[1]], x[0]))
+    return final 
+   
+
+        
+
+
+
+
+        
+
+
+    
+
+
 '''
 generates a full quiz of 20 questions from a given input query
 '''
@@ -111,4 +180,5 @@ generates a full quiz of 20 questions from a given input query
 
 def generateQuiz(query):
     valid = getValidQuestions(query)
-    return getTwentyRandQuestions(valid)
+    #return getTwentyRandQuestions(valid)
+    return getMostSimilar(valid)
